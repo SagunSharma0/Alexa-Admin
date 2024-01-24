2af20824fa41faf17500fcbc93c3e2fd3b51042b
SagunSharma0 <94883818+SagunSharma0@users.noreply.github.com>
2024-01-24 20:44:13 +0545

    billbord fixed

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -8,9 +8,7 @@ import { DataTable } from "@/components/ui/data-table";
 import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
 import { ApiList } from "@/components/ui/api-list";
-import { BillboardColumn, columns } from "./columns";
-
-
+import { columns, BillboardColumn } from "./columns";
 
 interface BillboardClientProps {
   data: BillboardColumn[];
@@ -25,7 +23,8 @@ export const BillboardClient: React.FC<BillboardClientProps> = ({
   return (
     <>
       <div className="flex items-center justify-between">
-        <Heading title={`Billboards (${data.length})`} description="Manage billboards for your store" />
+        <Heading title={`Billboards (${data.length})`} 
+        description="Manage billboards for your store" />
         <Button onClick={() => router.push(`/${params.storeId}/billboards/new`)}>
           <Plus className="mr-2 h-4 w-4" /> Add New
         </Button>
